badeaa0ebd6bd060b2d8e2ceac566860e7443beb
Everton Barbosa Jr <evertonjrccomp@gmail.com>
2019-09-29 01:26:12 -0300

    Adiciona doc para método mágico find

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -4,6 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Fornecedor
+ * @package App
+ *
+ * @method \App\Fornecedor find($id)
+ */
 class Fornecedor extends Model
 {
     protected $table = 'fornecedores';
